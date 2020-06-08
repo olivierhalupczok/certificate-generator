4997a0db2e37695b2723161172aae2e78685614f
Olivier <olivierkus88@gmail.com>
2020-06-09 00:18:07 +0200

    Updates

---
+++ precache-manifest.2f8455f84d314b8116ddf2f8e724519d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebb66731aa63d8ae511eb31e67caf1f",
+    "url": "/certificate-generator/index.html"
+  },
+  {
+    "revision": "635d02e805dd4cc1ddc7",
+    "url": "/certificate-generator/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "b54350d77aaef1071ea1",
+    "url": "/certificate-generator/static/css/main.6b562b0b.chunk.css"
+  },
+  {
+    "revision": "635d02e805dd4cc1ddc7",
+    "url": "/certificate-generator/static/js/2.3e6534d7.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/certificate-generator/static/js/2.3e6534d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54350d77aaef1071ea1",
+    "url": "/certificate-generator/static/js/main.0c44f03e.chunk.js"
+  },
+  {
+    "revision": "629cf1e350b063528caf",
+    "url": "/certificate-generator/static/js/runtime-main.3be47103.js"
+  },
+  {
+    "revision": "7c6eb4aaf0727ebaf8fab95be7324238",
+    "url": "/certificate-generator/static/media/winner_.7c6eb4aa.svg"
+  }
+]);
